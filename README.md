2766915733f7265b86bbb1d622dbd31da11beac2
Alif Zulkifly <alifzulkifly1101@gmail.com>
2024-11-05 18:47:52 +0700

    add balanced bracket code and update README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# gitsid_test_mobile
-GITS ID Problem Solving Test - Mobile (Dart)
+# GITS ID Problem Solving Test - Mobile (Dart)
+
+## Analisis Kompleksitas Kode Program Balanced Brackets
+
+### Penjelasan Time Complexity
+
+Time Complexity pada program ini adalah O(n), dimana n adalah panjang dari string input. Program berjalan dengan linear time karena harus memeriksa setiap karakter dalam string input tepat satu kali. Setiap karakter yang diperiksa melakukan operasi dasar seperti penambahan ke stack atau pengecekan pasangan bracket yang membutuhkan waktu konstan O(1).
+
+### Penjelasan Space Complexity
+
+Space Complexity pada program ini adalah O(n) karena menggunakan struktur data stack untuk menyimpan bracket. Dalam worst scenario, stack akan menyimpan setengah dari jumlah karakter input (semua bracket pembuka).
---
+++ balanced_bracket.dart
@@ -0,0 +1,46 @@
+void main() {
+  final testCases = [
+    "{ [ ( ) ] }",
+    "{ [( ])}",
+    "{(([ ])[ ])[ ]}",
+  ];
+
+  for (var test in testCases) {
+    print("Input: $test");
+    print("Output: ${checkBrackets(test)}\n");
+  }
+}
+
+final Map<String, String> bracketPairs = {
+  "{": "}",
+  "[": "]",
+  "(": ")",
+};
+
+final List<String> bracketClosing =
+    bracketPairs.entries.map((entry) => entry.value).toList();
+
+String checkBrackets(String input) {
+  return isBalancedBrackets(input) ? "YES" : "NO";
+}
+
+bool isBalancedBrackets(String input) {
+  List<String> stack = [];
+
+  for (int i = 0; i < input.length; i++) {
+    String current = input[i];
+
+    if (bracketPairs.containsKey(current)) {
+      stack.add(current);
+    } else if (bracketClosing.contains(current)) {
+      if (stack.isEmpty) return false;
+
+      String lastOpening = stack.removeLast();
+      if (bracketPairs[lastOpening] != current) {
+        return false;
+      }
+    }
+  }
+
+  return stack.isEmpty;
+}
